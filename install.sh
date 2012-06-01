d4f5c1e1a891d74c4194d56368a2719486e812ea
alateas <dmitrymashkin@gmail.com>
2012-06-01 10:11:49 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,48 @@
+function check_root() {
+    if [ "$(id -u)" != "0" ]; then
+        error_msg "ERROR! You must execute the script as the 'root' user."
+    fi
+}
+
+function check_sudo() {
+    if [ ! -n ${SUDO_USER} ]; then
+        error_msg "ERROR! You must invoke the script using 'sudo'."
+    fi
+}
+
+function check_ubuntu() {
+    if [ "${1}" != "" ]; then
+        SUPPORTED_CODENAMES="${1}"
+    else
+        SUPPORTED_CODENAMES="all"
+    fi
+
+    # Source the lsb-release file.
+    lsb
+
+    # Check if this script is supported on this version of Ubuntu.
+    if [ "${SUPPORTED_CODENAMES}" == "all" ]; then
+        SUPPORTED=1
+    else
+        SUPPORTED=0
+        for CHECK_CODENAME in `echo ${SUPPORTED_CODENAMES}`
+        do
+            if [ "${LSB_CODE}" == "${CHECK_CODENAME}" ]; then
+                SUPPORTED=1
+            fi
+        done
+    fi
+
+    if [ ${SUPPORTED} -eq 0 ]; then
+        error_msg "ERROR! ${0} is not supported on this version of Ubuntu."
+    fi
+}
+
+#checks
+check_root
+check_sudo
+check_ubuntu "all"
+
 #nginx
 apt-get -y install nginx
 rm -f /etc/nginx/nginx.conf
