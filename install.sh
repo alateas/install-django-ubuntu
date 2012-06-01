e05922903d52d7bc696c8b396581fbf987beb269
alateas <dmitrymashkin@gmail.com>
2012-06-01 10:14:24 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,97 @@
+# common ############################################################### START #
+sp="/-\|"
+log="${PWD}/`basename ${0}`.log"
+
+function error_msg() {
+    local MSG="${1}"
+    echo "${MSG}"
+    exit 1
+}
+
+function cecho() {
+    echo -e "$1"
+    echo -e "$1" >>"$log"
+    tput sgr0;
+}
+
+function ncecho() {
+    echo -ne "$1"
+    echo -ne "$1" >>"$log"
+    tput sgr0
+}
+
+function spinny() {
+    echo -ne "\b${sp:i++%${#sp}:1}"
+}
+
+function progress() {
+    ncecho "  ";
+    while [ /bin/true ]; do
+        kill -0 $pid 2>/dev/null;
+        if [[ $? = "0" ]]; then
+            spinny
+            sleep 0.25
+        else
+            ncecho "\b\b";
+            wait $pid
+            retcode=$?
+            echo "$pid's retcode: $retcode" >> "$log"
+            if [[ $retcode = "0" ]] || [[ $retcode = "255" ]]; then
+                cecho success
+            else
+                cecho failed
+                echo -e " [i] Showing the last 5 lines from the logfile ($log)...";
+                tail -n5 "$log"
+                exit 1;
+            fi
+            break 2;
+        fi
+    done
+}
+
+function progress_loop() {
+    ncecho "  ";
+    while [ /bin/true ]; do
+        kill -0 $pid 2>/dev/null;
+        if [[ $? = "0" ]]; then
+            spinny
+            sleep 0.25
+        else
+            ncecho "\b\b";
+            wait $pid
+            retcode=$?
+            echo "$pid's retcode: $retcode" >> "$log"
+            if [[ $retcode = "0" ]] || [[ $retcode = "255" ]]; then
+                cecho success
+            else
+                cecho failed
+                echo -e " [i] Showing the last 5 lines from the logfile ($log)...";
+                tail -n5 "$log"
+                exit 1;
+            fi
+            break 1;
+        fi
+    done
+}
+
+function progress_can_fail() {
+    ncecho "  ";
+    while [ /bin/true ]; do
+        kill -0 $pid 2>/dev/null;
+        if [[ $? = "0" ]]; then
+            spinny
+            sleep 0.25
+        else
+            ncecho "\b\b";
+            wait $pid
+            retcode=$?
+            echo "$pid's retcode: $retcode" >> "$log"
+            cecho success
+            break 2;
+        fi
+    done
+}
+
 function check_root() {
     if [ "$(id -u)" != "0" ]; then
         error_msg "ERROR! You must execute the script as the 'root' user."
@@ -37,6 +131,27 @@ function check_ubuntu() {
         error_msg "ERROR! ${0} is not supported on this version of Ubuntu."
     fi
 }
+
+function lsb() {
+    local CMD_LSB_RELEASE=`which lsb_release`
+    if [ "${CMD_LSB_RELEASE}" == "" ]; then
+        error_msg "ERROR! 'lsb_release' was not found. I can't identify your distribution."
+    fi
+    LSB_ID=`lsb_release -i | cut -f2 | sed 's/ //g'`
+    LSB_REL=`lsb_release -r | cut -f2 | sed 's/ //g'`
+    LSB_CODE=`lsb_release -c | cut -f2 | sed 's/ //g'`
+    LSB_DESC=`lsb_release -d | cut -f2`
+    LSB_ARCH=`dpkg --print-architecture`
+    LSB_MACH=`uname -m`
+    LSB_NUM=`echo ${LSB_REL} | sed s'/\.//g'`
+}
+
+function apt_update() {
+    ncecho " [x] Update package list "
+    apt-get -y update >>"$log" 2>&1 &
+    pid=$!;progress $pid
+}
+# common ################################################################# END #
 
 #checks
 check_root
